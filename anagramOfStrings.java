2f6cfe8373b5bacc173cf6eb25a3cc4133838055
Priyam <61192306+priyam-shah@users.noreply.github.com>
2020-04-10 01:48:06 +0530

    Create anagramOfStrings.java

---
+++ anagramOfStrings.java
@@ -0,0 +1,94 @@
+// method 1 ------------- O9n^2) ---------easy--------less-effective
+
+
+package arrays;
+public class AnagramOfString {
+
+	public static void main(String[] args) {
+		
+		
+		String a ="aab";
+		String b ="abc";
+		
+		boolean visited[] = new boolean[b.length()];  //boolean to mark a character when it is checked 
+		boolean isAnagram = false ;
+		
+		if(a.length()==b.length()) {                // basic check
+			for(int i=0; i< a.length() ;i++) {
+				
+				char c =a.charAt(i);
+				isAnagram = false ;
+				
+				for(int j=0; j<b.length();j++ ) {
+					
+					if(b.charAt(j)==c && !visited[j]) {   // compare every character of b with a 
+						visited[j]= true ;                  // if found mark true in visited
+						isAnagram = true ;
+						break;
+					}
+				}
+				if(!isAnagram) {
+					break;
+				}
+			}
+		
+		}
+		
+		if(isAnagram) {
+			System.out.println("anagram");
+		}
+		
+		else {
+			System.out.println("not anagram");
+		}
+	}
+
+}
+
+
+// method 2 ------------- using two arrays --- in one array lets say a iterate and find aski values of corrosponding characters 
+// .. with that values mark number of appearance of that character .. and finally compare two arrays 
+
+package arrays;
+
+public class Anagram2 {
+	public static void main(String[] args) {
+		
+		boolean isAnagram = true ;
+		String a ="listen";
+		String b ="silent";
+		
+		int al[] = new int[256];
+		int bl[] = new int[256];
+		
+		for(char c: a.toCharArray()) {
+			int index= (int)c;
+			al[index]++;
+		}
+		
+		for(char c: b.toCharArray()) {
+			int index= (int)c;
+			bl[index]++;
+		}
+		
+		 for(int i=0;i<256;i++) {
+			if(al[i] != bl[i]) {
+				isAnagram =false;
+				break;
+			}
+		 }
+		
+		if(isAnagram) {
+			System.out.println("anagram");
+		}
+		
+		else {
+			System.out.println("not anagram");
+		}
+		
+	}
+}
+
+
+// method-3 ------optimized version of send method --------
+
